83e3c780efa0323edecc3ee7fa0a7f347533b032
marti <martinagarciaa012@gmail.com>
2023-10-09 12:24:37 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Para cada archivo Main que se haga se tiene que ir al "Edit Run Configuration" como indicaba el video:
+
+https://www.youtube.com/watch?v=p_SUbcf0nCk
+
+![img.png](img.png)
+![img_1.png](img_1.png)
+Sin eso no va a funcionar y les va a tirar error en rojito.
+![img_2.png](img_2.png)
---
+++ src/MPJExample.java
@@ -0,0 +1,25 @@
+import mpi.*;
+public class MPJExample {
+    public static void main(String[] args) throws MPIException {
+        MPI.Init(args);
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        if (rank == 0) {
+            // Proceso maestro
+            int[] dataToSend = {1, 2, 3, 4, 5};
+            MPI.COMM_WORLD.Send(dataToSend, 0, dataToSend.length, MPI.INT, 1, 0);
+            System.out.println("Proceso " + rank + " envió datos al proceso 1.");
+        } else if (rank == 1) {
+            // Proceso esclavo
+            int[] receivedData = new int[5];
+            MPI.COMM_WORLD.Recv(receivedData, 0, receivedData.length, MPI.INT, 0, 0);
+            System.out.print("Proceso " + rank + " recibió datos: ");
+            for (int i : receivedData) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        MPI.Finalize();
+    }
+}
+
